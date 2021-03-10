4b47d678d120f8220829e6447bc13440f141eabe
hftamayo <hftamayo@gmail.com>
2021-03-10 17:37:37 -0600

    uso de BrowserRouter para desplegar Link

--- src/Paginas/Menu/index.js
+++ src/Paginas/Menu/index.js
@@ -1,45 +1,67 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, BrowserRouter } from 'react-router-dom';
 
 function Menu(){
     return(
         <div>
             <ul>
                 <li>
+                <BrowserRouter>
                     <Link to="/basicos">01 Basicos</Link>
                     {/*jsx, componentes, props, state, 
                     eventos y metodos, components naming,
                     hooks, stateless, stateful */}
+                </BrowserRouter>                    
                 </li>
                 <li>
+                <BrowserRouter>                    
                     <Link to="/cvida">02 Ciclo de Vida</Link>
+                </BrowserRouter>                    
                 </li>
                 <li>
+                <BrowserRouter>                    
                     <Link to="/rutas">03 Routing</Link>
+                </BrowserRouter>                    
                 </li>                
                 <li>
+                <BrowserRouter>                    
                     <Link to="/rutas">04 Redux</Link>
+                </BrowserRouter>                    
                 </li>                                
                 <li>
+                <BrowserRouter>                    
                     <Link to="/rutas">05 ReduxToolkit</Link>
+                </BrowserRouter>                    
                 </li>                                
                 <li>
-                    <Link to="/rutas">06 API Fetch: Axios</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">06 API Fetch: Axios</Link>
+                    </BrowserRouter>                        
                 </li>                                    
                 <li>
-                    <Link to="/rutas">07 API Fetch: ReactQuery</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">07 API Fetch: ReactQuery</Link>
+                    </BrowserRouter>                    
                 </li>                                         
                 <li>
-                    <Link to="/rutas">08 CRUD Single Entity</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">08 CRUD Single Entity</Link>
+                    </BrowserRouter>                    
                 </li>                                      
                 <li>
-                    <Link to="/rutas">09 CRUD OneToMany</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">09 CRUD OneToMany</Link>
+                    </BrowserRouter>                        
                 </li>                                  
                 <li>
-                    <Link to="/rutas">10 Login</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">10 Login</Link>
+                    </BrowserRouter>                        
                 </li>                         
                 <li>
-                    <Link to="/rutas">11 ShoppingCart</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">11 ShoppingCart</Link>
+                    </BrowserRouter>                        
                 </li>
             </ul>
         </div>
